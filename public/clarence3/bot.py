84180d6e5e2862d64dceef8963f0fe303c32d2af
Akhildivakar <adiv21r@royallatin.org>
2026-02-12 13:22:23 +0000

    Started on bot code for Clarence.

---
+++ public/clarence3/bot.py
@@ -0,0 +1,39 @@
+COMPONENTS = [
+    {
+        "name": "Base",
+        "port": "A",
+        "min": 0,
+        "max": 360
+    },
+    {
+        "name": "Secondary Arm",
+        "port": "D",
+        "min": 204,
+        "max": 311
+    },{
+        "name": "Primary Arm",
+        "port": "E",
+        "min": 8,
+        "max": 149
+    },{
+        "name": "Gripper",
+        "port": "C",
+        "min": 136,
+        "max": 238
+    }
+]
+
+def run_component(index, percentage, leverage = 5):
+    component = COMPONENTS[index]
+    port = component["port"]
+    min = component["min"] + leverage
+    max = component["max"] - leverage
+    
+    true_percent = percentage / 100
+    
+    degree = int(min + ((max - min) * true_percent))
+    print(component["name"], degree)
+    
+data = [50, 50, 50, 50]
+for i in range(len(data)):
+    run_component(i, data[i])
